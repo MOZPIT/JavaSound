b9ce0479b9b432aac942ecd865bb2287fed5f127
Mo Sanchez <mo@vitals.agency>
2019-11-06 12:13:58 -0500

    Adding all audio scripts in from unitds 4.1 to 4.6

---
+++ src/EchoFilter.java
@@ -0,0 +1,60 @@
+
+/*
+ * The EchoFilter class is a SoundFilter that emulates an echo.
+ */
+
+public class EchoFilter extends SoundFilter {
+	private short[] delayBuffer;
+	private int delayBufferPos;
+	private float decay;
+	
+	/*
+	 * Creates an EchoFilter with the specified number of delay samples and the specified decay rate.
+	 * <p>The number of decay samples specifies how long before the echo is initially heard. For a 1 second echo with mono,
+	 * 44100Hz sound, use 44100 delay samples.
+	 * <p>The decay value is how much the echo has decayed from the source. A decay value of .5 means the echo heard is half as loud as the source. 
+	 */
+	public EchoFilter(int numDelaySamples, float decay) {
+		delayBuffer = new short[numDelaySamples];
+		this.decay = decay;
+	}
+	
+	public int getRemainingSize() {
+		float finalDecay = 0.01f;
+		//derived from Math.pow(decay,x) <= finalDecay
+		int numRemainingBuffers = (int)Math.ceil(Math.log(finalDecay) / Math.log(decay));
+		int bufferSize = delayBuffer.length * 2;
+		
+		return bufferSize * numRemainingBuffers;
+	}
+	
+	/*
+	 * Clears this EchoFilter's internal delay buffer.
+	 */
+	public void reset() {
+		for(int i=0; i<delayBuffer.length; i++) {
+			delayBuffer[i] = 0;
+		}
+		delayBufferPos = 0;
+	}
+	
+	/*
+	 * Filters the sound samples to add an echo. The samples played are added to the sound in the delay buffer multiplied by the decay rate. The result is then stored in the 
+	 * delay buffer multiplied by the decay rate. The result is then stored in the delay buffer, so multiple echoes are heard.
+	 */
+	public void filter(byte[] samples, int offset, int length) {
+		for(int i=offset; i<offset+length; i+=2) {
+			//update the sample
+			short oldSample = getSample(samples, i);
+			short newSample = (short)(oldSample + decay * delayBuffer[delayBufferPos]);
+			setSample(samples, i, newSample);
+			
+			//update the delay buffer
+			delayBuffer[delayBufferPos] = newSample;
+			delayBufferPos++;
+			if(delayBufferPos == delayBuffer.length) {
+				delayBufferPos = 0;
+			}
+		}
+	}
+}
---
+++ src/EchoFilterTest.java
@@ -0,0 +1,31 @@
+import java.io.*;
+
+
+	/*
+	 * An example of playing a sound with an echo filter.
+	 * @see EchoFilter
+	 * @see SimpleSoundPlayer
+	 */
+public class EchoFilterTest{
+		@SuppressWarnings("resource")
+		public static void main(String[] args) {
+			//load the sound
+			SimpleSoundPlayer sound = new SimpleSoundPlayer("sound/voice.wav");
+			
+			//create the sound stream
+			InputStream is = new ByteArrayInputStream(sound.getSamples());
+			
+			//create an echo with a 11025-sample buffer
+			//(1/4 sec for 44100Hz sound) and a 60% decay
+			EchoFilter filter = new EchoFilter(11025, .6f);
+			
+			//create the filtered sound stream
+			is = new FilteredSoundStream(is, filter);
+			
+			//play sound
+			sound.play(is);
+			
+			//due to bug in Java Sound, explicitly exit the VM.
+			System.exit(0);
+		}
+}
